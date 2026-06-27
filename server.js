d82d73ebcd1d9162100d6f6a7bf5d352d1869aa6
Santoshi <srbaslingol@gmail.com>
2026-06-27 19:59:41 +0530

    server modified

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import "./server/config/env.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 import connectDB from "./server/config/db.js";
 import app from "./server/app.js";
-
-
 
 connectDB();
 
